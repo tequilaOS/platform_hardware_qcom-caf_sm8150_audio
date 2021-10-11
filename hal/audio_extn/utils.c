62ebfcd0e1e28cbe68ee5647dc078be92d97d180
K A R T H I K <karthik.lal558@gmail.com>
2021-10-11 11:15:48 +0530

    hal: Drop compress_get_metadata(out->compr, &metadata)
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: Ic47f4082b4b3849289d48a63c98c1a0db79b85af

--- hal/audio_extn/utils.c
+++ hal/audio_extn/utils.c
@@ -2289,7 +2289,6 @@ int audio_extn_utils_compress_get_dsp_latency(struct stream_out *out)
         }
 
         metadata.key = SNDRV_COMPRESS_PATH_DELAY;
-        ret = compress_get_metadata(out->compr, &metadata);
         if(ret) {
             ALOGE("%s::error %s", __func__, compress_get_error(out->compr));
             goto exit;
@@ -2338,7 +2337,6 @@ int audio_extn_utils_compress_set_render_mode(struct stream_out *out)
         ret = 0;
         goto exit;
     }
-    ret = compress_set_metadata(out->compr, &metadata);
     if(ret) {
         ALOGE("%s::error %s", __func__, compress_get_error(out->compr));
     }
@@ -2404,7 +2402,6 @@ int audio_extn_utils_compress_set_clk_rec_mode(
 
     ALOGD("%s:: clk recovery mode %d",__func__, metadata.value[0]);
 
-    ret = compress_set_metadata(out->compr, &metadata);
     if(ret) {
         ALOGE("%s::error %s", __func__, compress_get_error(out->compr));
     }
@@ -2466,7 +2463,6 @@ int audio_extn_utils_compress_set_render_window(
     metadata.value[3] = \
             (0xFFFFFFFF00000000 & render_window->render_we) >> 32; /* msb*/
 
-    ret = compress_set_metadata(out->compr, &metadata);
     if(ret) {
         ALOGE("%s::error %s", __func__, compress_get_error(out->compr));
     }
@@ -2523,7 +2519,6 @@ int audio_extn_utils_compress_set_start_delay(
     metadata.value[1] = \
             (0xFFFFFFFF00000000 & delay_param->start_delay) >> 32; /* msb*/
 
-    ret = compress_set_metadata(out->compr, &metadata);
     if(ret) {
         ALOGE("%s::error %s", __func__, compress_get_error(out->compr));
     }
@@ -2553,7 +2548,6 @@ int audio_extn_utils_compress_get_dsp_presentation_pos(struct stream_out *out,
     ALOGV("%s:: Quering DSP position with clock id %d",__func__, clock_id);
     metadata.key = SNDRV_COMPRESS_DSP_POSITION;
     metadata.value[0] = clock_id;
-    ret = compress_get_metadata(out->compr, &metadata);
     if (ret) {
         ALOGE("%s::error %s", __func__, compress_get_error(out->compr));
         ret = -errno;
@@ -2796,7 +2790,6 @@ int audio_extn_utils_compress_enable_drift_correction(
     metadata.value[0] = drift->enable;
     out->drift_correction_enabled = drift->enable;
 
-    ret = compress_set_metadata(out->compr, &metadata);
     if(ret) {
         ALOGE("%s::error %s", __func__, compress_get_error(out->compr));
         out->drift_correction_enabled = false;
@@ -2851,7 +2844,6 @@ int audio_extn_utils_compress_correct_drift(
     metadata.value[1] = \
              (0xFFFFFFFF00000000 & drift_param->adjust_time) >> 32; /* msb*/
 
-    ret = compress_set_metadata(out->compr, &metadata);
     if(ret)
         ALOGE("%s::error %s", __func__, compress_get_error(out->compr));
 exit:
